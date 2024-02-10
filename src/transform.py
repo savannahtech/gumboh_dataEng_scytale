7c47fd38e6c94d093e8a53b30b479a26a9858c6a
GYASI <GYASI@DESKTOP-K8EF16B>
2024-02-10 09:04:03 +0000

    updates to requirements, update read me

--- src/transform.py
+++ src/transform.py
@@ -1,56 +1,77 @@
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, BooleanType
-from pyspark.sql.functions import from_unixtime, col
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, BooleanType, DateType
+from pyspark.sql.functions import substring, from_unixtime, col, when, split
+import os
 
-spark = SparkSession.builder.appName('GithubETL').getOrCreate()
 
-def transform_data(github_token):
+# Initializing the SparkSession
+if SparkSession.getActiveSession():
+    spark = SparkSession.getActiveSession()
+else:
+    spark = SparkSession.builder.appName("readJSONdata").getOrCreate()
 
-    # Define the schema for the JSON data
+
+def transform_data():
+    # Define the schema
     json_schema = StructType([
-        StructField("created_at", StringType(), True),
-        StructField("raw", StructType([
-            StructField("full_name", StringType(), True),
-            StructField("id", StringType(), True),
-            StructField("name", StringType(), True),
-            StructField("owner", StructType([
-                StructField("login", StringType(), True)
-            ]), True)
+        StructField("id", StringType(), True),
+        StructField("name", StringType(), True),
+        StructField("full_name", StringType(), True),
+        StructField("owner", StructType([
+            StructField("login", StringType(), True)
         ]), True),
-        StructField("language", StringType(), True),
         StructField("num_prs", IntegerType(), True),
         StructField("num_prs_merged", IntegerType(), True),
         StructField("merged_at", StringType(), True),
-        # Add more fields if needed
+        StructField("updated_at", StringType(), True),
     ])
 
-    # Read all extract JSON files with the specified schema
-    extract_df = spark.read.schema(json_schema).json('data/raw_data')
+    # Defining the path to our merged JSON file
+    json_file_path = "data/raw_data/repo_zote.json"
 
-    # Transformation
-    transformed_df = extract_df.withColumn("created_date", F.unix_timestamp(col("created_at"), "yyyy-MM-dd"))
-    transformed_df = transformed_df.withColumn("repo_name", F.split(F.col("raw.full_name"), "/").getItem(0))
-    transformed_df = transformed_df.filter(
-        (col("num_prs") == col("num_prs_merged")) & (F.col("raw.owner.login").contains("scytale"))
-    )
+    # Using the defined json_schema to read the JSON file into a DataFrame
+    df = spark.read.json(json_file_path, schema=json_schema)
 
-    # Select required columns
-    result_df = transformed_df.select(
-        col("repo_name").alias("Organization Name"),
-        col("raw.id").alias("repository_id"),
-        col("raw.name").alias("repository_name"),
-        col("raw.owner.login").alias("repository_owner"),
-        col("num_prs"),
+    # Calculate the number of PRs for each repository
+    prs_count_df = df.groupBy("full_name").count().withColumnRenamed("count", "prs_count")
+
+    # Calculate the number of merged PRs for each repository
+    merged_prs_count_df = df.filter(col("merged_at").isNotNull()).groupBy("full_name").count().withColumnRenamed("count", "merged_prs_count")
+
+    df = df.join(prs_count_df, "full_name", "left").join(merged_prs_count_df, "full_name", "left")
+    df = df.fillna({"num_prs_merged": 0})
+
+    print("Total Count of Extracted JSON Data:", df.count())
+
+    #df.show()
+
+    # Perform schema transformation
+    select_df = df.select(
+        split(col("full_name"), "/").getItem(0).alias("Organization Name"),
+        col("id").alias("repository_id"),
+        col("name").alias("repository_name"),
+        col("owner.login").alias("repository_owner"),
+        col("prs_count").alias("num_prs"),
         col("num_prs_merged"),
-        from_unixtime(col("merged_at"), 'yyyy-MM-dd').alias("merged_at"),
-        ((col("num_prs") == col("num_prs_merged")) & F.col("raw.owner.login").contains("scytale")).alias("is_compliant")
+        when(col("num_prs_merged") == 0, "N/A").otherwise(col("updated_at")).alias("merged_at"),
+        when((col("num_prs") == col("merged_prs_count")) & (col("owner.login").contains("scytale")), True).otherwise(False).alias("is_compliant")
     )
+
+    # Print the number of rows in the transformed DataFrame
+    print("Total Count of Transformed Rows from JSON Data:", select_df.count())
 
     # Write to parquet file
-    result_df.write.mode("overwrite").parquet("data/results")
+    select_df.write.mode("overwrite").parquet("data/results")
+
+    # Show the resulting DataFrame
+    #select_df.show()
+
+    # close sparksession
+    spark.stop()
 
 if __name__ == '__main__':
     transform_data()
+
 
